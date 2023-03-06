87628bc5c395341b06cae27756e48c2990179979
Paulina <paula232@inbox.lt>
2023-03-06 11:39:16 +0200

    create some blade, css ir routes

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,70 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>E-shop drabuziu parduotuve</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/startCss.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="http://laravel-diplom-1.rdavydov.ru">Elektronine drabuziu parduotuve</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li ><a href="http://http://eShopForEveryone.lt">Visi produktai</a></li>
+                <li  class="active" ><a href="eShopForEveryone.lt/categories">Kategorijos</a>
+                </li>
+                <li ><a href="http://http://eShopForEveryone.lt/basket">I krepseli</a></li>
+                <li><a href="http://http://eShopForEveryone.lt/reset">Grazinti projekta i pradine padeti</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="http://http://eShopForEveryone.lt/admin/home">Admino panele</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<div class="container">
+    <div class="starter-template">
+        <div class="panel">
+            <a href="http://http://eShopForEveryone.lt/men">
+                <img src="http://http://eShopForEveryone.lt/storage/categories/mens.jpg">
+                <h2>Vyriski drabuziai</h2>
+            </a>
+            <p>
+                Cia bus vyru drabuziai            </p>
+        </div>
+        <div class="panel">
+            <a href="http://http://eShopForEveryone.lt/portable">
+                <img src="http://http://eShopForEveryone.lt/storage/categories/portable.jpg">
+                <h2>Moteryski drabvuziai</h2>
+            </a>
+            <p>
+                Cia bus moteriski drabuziai
+            </p>
+        </div>
+        <div class="panel">
+            <a href="http://http://eShopForEveryone.lt/women">
+                <img src="http://http://eShopForEveryone.lt/storage/categories/appliance.jpg">
+                <h2>Vaiku drabuziai</h2>
+            </a>
+            <p>
+                Cia vaikiski drabuziai
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,1 +1,49 @@
-cia bus produkto blaidas
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Elektronine drabuziu parduotuve</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/startCss.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="http://eShopForEveryone.lt">E-shop parduotuve</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li ><a href="http://eShopForEveryone.lt">Visi produktai</a></li>
+                <li ><a href="http://eShopForEveryone.lt/categories">Kategorijos</a>
+                </li>
+                <li ><a href="http://eShopForEveryone.lt/basket">I krepseli</a></li>
+                <li><a href="http://eShopForEveryone.lt/reset">Grazinti projekta i pradine padeti</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="http://eShopForEveryone.lt/admin/home">Admino panele</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<div class="container">
+    <div class="starter-template">
+        <h1>Vyr.kepure</h1>
+        <p>Kaina: <b>7 Eur</b></p>
+        <img src="http://eShopForEveryone.lt/storage/products/iphone_x.jpg">
+        <p>Puikiai prie galvos priglundanti kepure</p>
+        <a class="btn btn-success" href="http://eShopForEveryone.lt/basket/1/add">I krepseli</a>
+    </div>
+</div>
+</body>
+</html>
