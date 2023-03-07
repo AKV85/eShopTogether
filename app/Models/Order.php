cc1b425b061e2f6bb41f70c4f4e4de3157871e6a
AKV85 <a.kotov.laknojus@gmail.com>
2023-03-07 20:34:55 +0200

    Pridejau remove funkcija i basketControlleri ir patiuningavau add f.-ja.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,8 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+
+//    Funkcija "products" apibrėžia daugybę-į-daugybę ryšį tarp "Order" ir "Product" modelių. Tai reiškia, kad
+// kiekvienas krepšelio užsakymas (Order) gali turėti daug produktų (Product) ir kiekvienas produktas gali būti
+// priskirtas daugybei krepšelio užsakymų. Šis ryšys yra apibrėžtas naudojant Laravel Eloquent "belongsToMany" metodą,
+// kuris grąžina ryšio objektą, leidžiantį naudoti ryšio metodus. "withPivot" metodas nurodo papildomą lauką
+// (pavadinimu "count"), kuris yra saugomas pivot lentelėje. Pivot lentelė yra tarpinė lentelė, kuri saugo papildomus
+// laukus, kuriuos galima priskirti daugybei-į-daugybę ryšių. Šiuo atveju, "count" laukas nurodo, kiek vnt. konkretus
+// produktas yra priskirtas konkrečiam krepšelio užsakymui."withTimestamps" metodas nustato, kad pivot lentelė
+// automatiškai pildys sukūrimo ir atnaujinimo datos laukus, t.y., šie duomenys bus automatiškai užpildyti kiekvieną
+// kartą, kai bus sukuriamas ar atnaujinamas ryšys.
     public function products()
     {
-        return $this->belongsToMany(Product::class);
+        return $this->belongsToMany(Product::class)->withPivot('count')->withTimestamps();
+    }
+
+
+//    Funkcija "getFullPrice" naudojama skaičiuoti krepšelio užsakymo bendrąją kainą. Tai daroma pereinant per visus
+// produktus krepšelyje ir pridedant kainą už kiekvieną produktą, dauginant ją iš jo kiekio (naudojant
+// "getPriceForCount" metodą iš "Product" modelio). Galiausiai, funkcija grąžina visų produktų bendrąją kainą.
+    public function getFullPrice()
+    {
+        $sum = 0;
+        foreach ($this->products as $product) {
+            $sum += $product->getPriceForCount();
+        }
+        return $sum;
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -14,5 +15,12 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPriceForCount() {
+        if (!is_null($this->pivot)) {
+            return $this->pivot->count * $this->price;
+        }
+        return $this->price;
     }
 }
