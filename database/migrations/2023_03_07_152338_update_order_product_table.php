c2114534ccc6e55bc89b697e83860c79a07946ed
Paulina <paula232@inbox.lt>
2023-03-07 17:31:17 +0200

    prideta nauja migracija update_order_product

---
+++ database/migrations/2023_03_07_152338_update_order_product_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+//        Šis kodas naudojamas pridėti naują stulpelį į tarpinę lentelę, susijusią su ryšiu tarp Order ir Product
+//        modelių. Šiuo atveju naujas stulpelis yra count, kuris saugos produkto kiekį, pridėtą į krepšelį.
+        Schema::table('order_product', function (Blueprint $table) {
+            $table->integer('count')->default(1)->after('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
