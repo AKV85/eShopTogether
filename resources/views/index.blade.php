0d41459f762f5b3dbc6ad00b5ce0d47509ea33d1
Paulina <paula232@inbox.lt>
2023-03-09 10:48:57 +0200

    pakeistas koliazas ir tekstas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,15 +3,14 @@
 @section('title', 'Pagrindinis')
 
 @section('content')
-    <div style="font-size: 40px; color: rgb(62,250,5);">
+    <div style="font-size: 40px; color: #faf7f8;">
+    Sveiki apsilankę mano puslapyje!
+    Trumpai papasakosiu apie save ir ka čia galite rasti :)
 
-        Sveiki apsilankę mano puslapyje!
-        Trumpai papasakosiu apie save ir ka čia galite rasti :)
-
-        AKS - Kodėl AKS?
-        Nes tai mano inicialai - Aurelija Klimaitė Stankevičienė.
-        O Design - nes tai mano kurtas dizainas.
-        Paprasta ir patogu.
+    AKS - Kodėl AKS?
+    Nes tai mano inicialai - Aurelija Klimaitė Stankevičienė.
+    O Design - nes tai mano kurtas dizainas.
+    Paprasta ir patogu.
 
     Esu dviejų vaikų mama, todėl man labai aktuali mano profesija - siuvėja.
     Vaikai labai greit auga ir reikia vis naujų rūbelių, o kai mama moka siūti - tai ne problema!
@@ -19,5 +18,6 @@
     Todėl daugelis mano darbelių yra skirta vaikams, nors siuvu ir suaugusiems.
 
     Taip pat nuotraukų koliažą kurį matote - mano pirmieji darbai.
+
 
 @endsection
