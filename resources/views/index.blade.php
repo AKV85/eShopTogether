d7087ba8abca6c7ed1d3691f32bfe4ce1faa01cd
Paulina <paula232@inbox.lt>
2023-03-10 16:01:09 +0200

    pakeistas stilius

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Pagrindinis')
 
 @section('content')
-    <div style="font-size: 40px; color: #faf7f8;">
+    <div style="font-size: 45px; color: #110f0f;">
     Sveiki apsilankę mano puslapyje!
     Trumpai papasakosiu apie save ir ka čia galite rasti :)
 
@@ -16,8 +16,6 @@
     Vaikai labai greit auga ir reikia vis naujų rūbelių, o kai mama moka siūti - tai ne problema!
     Mano tikslas ne tik savo vaikučius papuošti, bet ir Jūsų.
     Todėl daugelis mano darbelių yra skirta vaikams, nors siuvu ir suaugusiems.
-
-    Taip pat nuotraukų koliažą kurį matote - mano pirmieji darbai.
 
 
 @endsection
